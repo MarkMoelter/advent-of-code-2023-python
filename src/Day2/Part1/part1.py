dbf59cdf553d5e21d6e3c52f2bd5a9d180c98adf
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-02 22:31:12 -0500

    Add part1.py

---
+++ src/Day2/Part1/part1.py
@@ -0,0 +1,5 @@
+class Part1:
+    def __init__(self, num_rgb: tuple[int, int, int]) -> None:
+        self.num_red = num_rgb[0]
+        self.num_green = num_rgb[1]
+        self.num_blue = num_rgb[2]
