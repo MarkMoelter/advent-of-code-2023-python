5b626711d1b7cdac809e6c26746b5d5691033751
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-02 23:05:22 -0500

    Add method to check if game is possible

--- src/Day2/Part1/part1.py
+++ src/Day2/Part1/part1.py
@@ -1,5 +1,19 @@
+from Day2.Part1.game import Game
+
+
 class Part1:
-    def __init__(self, num_rgb: tuple[int, int, int]) -> None:
+    def __init__(self, num_rgb: tuple[int, int, int], input_list: list[str]) -> None:
         self.num_red = num_rgb[0]
         self.num_green = num_rgb[1]
         self.num_blue = num_rgb[2]
+        self.input_list = input_list
+
+    def is_game_possible(self, game: Game) -> bool:
+        """
+        Check if the game is possible.
+        :param game: The game ot check.
+        :return: True if the game is possible
+        """
+        return (self.num_red >= game.largest_values["red"]
+                and self.num_green >= game.largest_values["green"]
+                and self.num_blue >= game.largest_values["blue"])
