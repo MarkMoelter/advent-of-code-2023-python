b5c68fa28cd628143015e1210531ffd2bd538155
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-04 01:09:26 -0500

    Add logic to part.py and test with test_input.txt

--- src/Day4/Part1/part1.py
+++ src/Day4/Part1/part1.py
@@ -1,7 +1,37 @@
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
 
 class Part1:
-    pass
+    def __init__(self, input_file: list[str]) -> None:
+        self.cards = input_file
+
+    @staticmethod
+    def get_winning_numbers(card: str) -> list[int]:
+        pattern = r"[ \d]+(?=\|)"
+        return list(map(int, re.search(pattern, card).group().split()))
+
+    @staticmethod
+    def get_playable_numbers(card: str) -> list[int]:
+        pattern = r"(?<=\|)[ \d]+"
+        return list(map(int, re.search(pattern, card).group().split()))
+
+    def puzzle_1(self) -> int:
+        total = 0
+        for card in self.cards:
+            count = 0
+            for winning_num in self.get_winning_numbers(card):
+                for playable in self.get_playable_numbers(card):
+                    if winning_num == playable:
+                        # logger.debug(winning_num)
+                        count += 1
+
+            if count > 0:
+                total += pow(2, count - 1)
+            else:
+                total += 0
+            logger.debug(total)
+
+        return total
--- src/Day4/main.py
+++ src/Day4/main.py
@@ -1,9 +1,17 @@
 import logging
 
+from Day4.Part1.part1 import Part1
+from read_file import read_input_file
+
 
 def main():
     print("Hello world!")
     # part 1
+    input_file = read_input_file("test_input.txt")
+    p1 = Part1(input_file)
+    print(p1.get_winning_numbers(p1.cards[0]))
+    print(p1.get_playable_numbers(p1.cards[0]))
+    print(p1.puzzle_1())
     # part 2
 
 
