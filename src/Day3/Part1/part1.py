2272a2b3cbd1f4d938cf34d3bff43c72c0f17ea2
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-03 23:30:19 -0500

    Add puzzle 2 and test_input.txt to help debug

--- src/Day3/Part1/part1.py
+++ src/Day3/Part1/part1.py
@@ -1,7 +1,11 @@
+import logging
+import math
 import re
 
 from Day3.Part1.number import Number
 from Day3.Part1.symbol import Symbol
+
+logger = logging.getLogger(__name__)
 
 
 class Part1:
@@ -34,6 +38,18 @@ class Part1:
                 numbers.append(Number(int(match.group()), (match.span()[0], match.span()[1] - 1), y_pos))
         return numbers
 
+    @staticmethod
+    def is_symbol_near_number(symbol: Symbol, num: Number) -> bool:
+        """
+        Check if a symbol is near a number. 'Near' is defined as adjacent or diagonal to each other.
+        :param symbol: The symbol to check.
+        :param num: The number to check.
+        :return: True if the symbol is near the number, False otherwise.
+        """
+        near_symbol_vertically = num.y_pos - 1 <= symbol.y_pos <= num.y_pos + 1
+        near_symbol_horizontally = num.x_span[0] - 1 <= symbol.x_pos <= num.x_span[1] + 1
+        return near_symbol_vertically and near_symbol_horizontally
+
     def puzzle_1(self) -> int:
         """
         Get the sum of all the part numbers in the engine schematic.
@@ -42,8 +58,23 @@ class Part1:
         total = 0
         for num in self._numbers:
             for symbol in self._symbols:
-                near_symbol_vertically = num.y_pos - 1 <= symbol.y_pos <= num.y_pos + 1
-                near_symbol_horizontally = num.x_span[0] - 1 <= symbol.x_pos <= num.x_span[1] + 1
-                if near_symbol_horizontally and near_symbol_vertically:
+                if self.is_symbol_near_number(symbol, num):
                     total += num.value
+        return total
+
+    def puzzle_2(self) -> int:
+        total = 0
+        for symbol in self._symbols:
+            if symbol.char != "*":
+                continue
+
+            gear_nums = []
+            for num in self._numbers:
+                if self.is_symbol_near_number(symbol, num):
+                    gear_nums.append(num.value)
+
+                if len(gear_nums) == 2:
+                    total += math.prod(gear_nums)
+                    logger.debug(gear_nums)
+
         return total
--- src/Day3/main.py
+++ src/Day3/main.py
@@ -13,8 +13,11 @@ def main():
     pprint(p1.numbers[0:10])
     print(p1.puzzle_1())
     # part 2
+    input_file = read_input_file("test_input.txt")
+    print(Part1(input_file).puzzle_2())
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+    logging.disable(logging.DEBUG)
     main()
