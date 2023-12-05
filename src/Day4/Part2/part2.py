2b6f89c0706107525381c4064f7e3ac2a57239be
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-05 14:10:35 -0500

    Implement cache to speed up recursion.

--- src/Day4/Part2/part2.py
+++ src/Day4/Part2/part2.py
@@ -9,29 +9,42 @@ logger = logging.getLogger(__name__)
 class Part2(Part1):
     def __init__(self, input_file: list[str]) -> None:
         super().__init__(input_file)
+        self.cache = dict()
 
     def puzzle_2(self) -> int:
         total = 0
         for card in self.cards:
             logger.debug(f"Original card: {card.card_id}")
-            total += self.get_ticket_count(card, 0)
+            total += self.get_number_of_cards_generated(card)
             logger.debug(f"Running total: {total}\n")
+            logger.debug(f"Final cache: {self.cache}")
 
         return total
 
-    def get_ticket_count(self, card: Card, count: int) -> int:
+    def get_number_of_cards_generated(self, card: Card) -> int:
         """
-        Get the number of cards generated from the cards winnings
-        :param count: The running total.
-        :param card: The card.
-        :return: The number of cards generated from the cards winnings.
+        Get the total number of cards generated for puzzle 2.
+        :param card: The initial card, also the top node in the tree.
+        :return: The number of cards generated.
         """
-        count += 1
-        logger.debug(f" -> ID: {card.card_id} -> {card.numbers_that_won} -> Count: {count}")
-        if len(card.numbers_that_won) == 0:
-            return count
+        # Check if the node has already been cached
+        if card.card_id in self.cache:
+            cached_count = self.cache[card.card_id]
+            logger.debug(f"Using cache for card: {card.card_id}. Cached count: {cached_count}")
+            return cached_count
 
+        # Check if the card is at the bottom of the tree
+        logger.debug(f"Card not cached, ID: {card.card_id} -> {card.numbers_that_won}")
+        if len(card.numbers_that_won) == 0:
+            self.cache[card.card_id] = 1
+            return 1
+
+        # Iterate through all children of the node
+        count = 1
         for card2 in self.cards[card.card_id: card.card_id + len(card.numbers_that_won)]:
-            count = self.get_ticket_count(card2, count)
+            count += self.get_number_of_cards_generated(card2)
+
+        if card.card_id not in self.cache:
+            self.cache[card.card_id] = count
 
         return count
