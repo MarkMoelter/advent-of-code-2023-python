b6214a592ad693930689e163243024b74e31f12f
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-06 12:01:54 -0500

    Add method to map each input value to its destination position
     Time and memory complexity should be O(n) where n is the length of the transformation mapping list.

--- src/Day5/part_1/part_1.py
+++ src/Day5/part_1/part_1.py
@@ -6,3 +6,16 @@ logger = logging.getLogger(__name__)
 class Part1:
     def __init__(self, input_input: list[str]) -> None:
         self.input_input = input_input
+
+    @staticmethod
+    def map_src_to_dest(input_val: int, transformation_map: list[tuple[int, int, int]]) -> int:
+        """
+        Map the input value from the source to the destination.
+        :param input_val: The value to map to the destination.
+        :param transformation_map: The map between source and destination.
+        :return: The destination position that it maps to.
+        """
+        for src, dest, _range in transformation_map:
+            if input_val in range(src, src + _range):
+                return input_val - src + dest
+        return input_val
