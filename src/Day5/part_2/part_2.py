19f58884977b2fa1cb655fba8957eee5571d443b
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-06 22:50:39 -0500

    Override seeds and solution methods for Part2

--- src/Day5/part_2/part_2.py
+++ src/Day5/part_2/part_2.py
@@ -1,8 +1,42 @@
 import logging
 
+from Day5.part_1 import Part1
+
 logger = logging.getLogger(__name__)
 
 
-class Part2:
-    def __init__(self, input_input: list[str]) -> None:
-        self.input_input = input_input
+class Part2(Part1):
+    def __init__(self, input_file: str) -> None:
+        super().__init__(input_file)
+
+    @property
+    def seeds(self) -> list[range]:
+        seed_values = super().seeds
+
+        is_starting_number = True
+        starting_number = 0
+        seed_ranges = []
+        for value in seed_values:
+            if is_starting_number:
+                starting_number = value
+                is_starting_number = False
+            else:
+                seed_ranges.append(range(starting_number, starting_number + value))
+                is_starting_number = True
+
+        return seed_ranges
+
+    def solution(self) -> int:
+        sol = 0
+
+        for r in self.seeds:
+            for i in r:
+                loc_val = self.transform_seed(i, 0)
+
+                # Add first value to seed
+                if sol == 0:
+                    sol = loc_val
+                # Add new min value if found
+                elif sol > loc_val:
+                    sol = loc_val
+        return sol
