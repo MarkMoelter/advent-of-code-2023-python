068d3070e51727df8ce3b50719d04b20ecfe406e
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-07 23:42:26 -0500

    Add race property to adjust the input to fit the puzzle.

--- src/Day6/main.py
+++ src/Day6/main.py
@@ -7,17 +7,18 @@ from Day6.part_2 import Part2
 
 def main():
     input_file = read_file_lines()
-    input_file = read_file_lines("test_input.txt")
+    # input_file = read_file_lines("test_input.txt")
 
     # part 1
     p1 = Part1(input_file)
-    print(p1.races)
+    print(p1.solution())
 
     # Only need to check the first half of the time, each half is a mirror of each other.
     # Check number of times you will beat record, dfs where end is > than the current record. -> Recursion
 
     # part 2
     p2 = Part2(input_file)
+    print(p2.solution())
 
 
 if __name__ == '__main__':
--- src/Day6/part_2/part_2.py
+++ src/Day6/part_2/part_2.py
@@ -1,6 +1,6 @@
 import logging
 
-from template.part_1 import Part1
+from src.Day6.part_1 import Part1
 
 logger = logging.getLogger(__name__)
 
@@ -8,3 +8,17 @@ logger = logging.getLogger(__name__)
 class Part2(Part1):
     def __init__(self, input_file: list[str]) -> None:
         super().__init__(input_file)
+        self._race = self.race
+
+    @property
+    def race(self) -> tuple[int, int]:
+        out = []
+        for line in self.input_file:
+            line = line.split()
+            line.pop(0)
+            out.append(int(''.join(line)))
+
+        return out[0], out[1]
+
+    def solution(self) -> int:
+        return self.get_faster_combos(self._race)
