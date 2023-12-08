bced218a10941489d5e80e9a91e50cf6cd950b99
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-07 23:57:01 -0500

    Add hands property and solution method

--- src/Day7/part_1/part_1.py
+++ src/Day7/part_1/part_1.py
@@ -6,3 +6,21 @@ logger = logging.getLogger(__name__)
 class Part1:
     def __init__(self, input_file: list[str]) -> None:
         self.input_file = input_file
+        self._hands = self.hands
+
+    @property
+    def hands(self) -> list[tuple[str, int]]:
+        out = []
+        for line in self.input_file:
+            hand, bid = line.split()
+            out.append((hand, int(bid)))
+
+        return out
+
+    def solution(self):
+        out = 0
+
+        for hand in self._hands:
+            pass
+
+        return out
