8c8f657f04c12674051dc0b44e5feea9eaf91955
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-07 22:40:51 -0500

    Add logic to get faster race combinations.

--- src/Day6/main.py
+++ src/Day6/main.py
@@ -11,7 +11,7 @@ def main():
 
     # part 1
     p1 = Part1(input_file)
-    print(p1.time_record)
+    print(p1.races)
 
     # Only need to check the first half of the time, each half is a mirror of each other.
     # Check number of times you will beat record, dfs where end is > than the current record. -> Recursion
--- src/Day6/part_1/part_1.py
+++ src/Day6/part_1/part_1.py
@@ -6,11 +6,10 @@ logger = logging.getLogger(__name__)
 class Part1:
     def __init__(self, input_file: list[str]) -> None:
         self.input_file = input_file
-        self._time_record = self.time_record
+        self._races = self.races
 
     @property
-    def time_record(self) -> list[tuple]:
-
+    def races(self) -> list[tuple[int, int]]:
         out = []
         for line in self.input_file:
             line = line.split()
@@ -18,3 +17,33 @@ class Part1:
             out.append(list(map(int, line)))
 
         return [i for i in zip(out[0], out[1])]
+
+    def solution(self) -> int:
+        out = 1
+        for race in self._races:
+            out *= self.get_faster_combos(race)
+        return out
+
+    @staticmethod
+    def get_faster_combos(race: tuple[int, int]) -> int:
+        """
+        Get the combinations in the race that are faster than the current record.
+        :param race: The race made up of the time and the current record.
+        :return: The combinations that are faster than the current record
+        """
+        faster_combos = 0
+        time, record = race
+
+        # Only deal with the first half of the time. It repeats in the second half.
+        for i in range(time // 2 + 1):
+            if (time - i) * i > record:
+                faster_combos += 1
+
+        # Multiply the result by 2 to account for only using the first half
+        faster_combos *= 2
+
+        # Even numbers will have 1 extra combo after multiplying. Subtract 1 to account for
+        if time % 2 == 0:
+            faster_combos -= 1
+
+        return faster_combos
