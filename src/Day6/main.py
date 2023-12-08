3592b5ee8101adeff6bdd0256903f3b0a8439db9
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-07 21:45:13 -0500

    Add time_record property

--- src/Day6/main.py
+++ src/Day6/main.py
@@ -1,8 +1,8 @@
 import logging
 
 from read_file import read_file_lines
-from template.part_1 import Part1
-from template.part_2 import Part2
+from Day6.part_1 import Part1
+from Day6.part_2 import Part2
 
 
 def main():
@@ -11,6 +11,7 @@ def main():
 
     # part 1
     p1 = Part1(input_file)
+    print(p1.time_record)
 
     # Only need to check the first half of the time, each half is a mirror of each other.
     # Check number of times you will beat record, dfs where end is > than the current record. -> Recursion
--- src/Day6/part_1/part_1.py
+++ src/Day6/part_1/part_1.py
@@ -6,3 +6,15 @@ logger = logging.getLogger(__name__)
 class Part1:
     def __init__(self, input_file: list[str]) -> None:
         self.input_file = input_file
+        self._time_record = self.time_record
+
+    @property
+    def time_record(self) -> list[tuple]:
+
+        out = []
+        for line in self.input_file:
+            line = line.split()
+            line.pop(0)
+            out.append(list(map(int, line)))
+
+        return [i for i in zip(out[0], out[1])]
