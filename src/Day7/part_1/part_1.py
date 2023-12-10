d8e3ae3ce1436e602a61c940c91e19b9666ce871
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-10 00:21:39 -0500

    Refactor to use Hand class

--- src/Day7/part_1/part_1.py
+++ src/Day7/part_1/part_1.py
@@ -1,6 +1,6 @@
 import logging
-from collections import Counter
 
+from .hand import Hand
 from .hand_type import HandType
 
 logger = logging.getLogger(__name__)
@@ -10,83 +10,55 @@ class Part1:
     def __init__(self, input_file: list[str]) -> None:
         self.input_file = input_file
         self._hands = self.hands
+        self._types_to_hand_list = self.types_to_hand_list
 
     @property
-    def hands(self) -> list[tuple[str, int]]:
+    def hands(self) -> list[Hand]:
         """
         Get a list of hands and bids from the input file.
         :return: The list of hands.
         """
         out = []
         for line in self.input_file:
-            hand, bid = line.split()
-            out.append((hand, int(bid)))
-
+            cards, bid = line.split()
+            out.append(Hand(cards, int(bid)))
         return out
 
-    @staticmethod
-    def get_hand_type(hand: str) -> HandType:
-        """
-        Get the type of the hand.
-
-        Possible types:
-
-        - five of a kind
-        - Four of a kind
-        - Full house
-        - Three of a kind
-        - Two pair
-        - One pair
-        - High card
-        :param hand: Consists of 5 cards.
-        :return: The type of the hand.
-        """
-        char_to_count = Counter(hand)
-        if 5 in char_to_count.values():
-            return HandType.FIVE_OF_A_KIND
-        if 4 in char_to_count.values():
-            return HandType.FOUR_OF_A_KIND
-        if 3 in char_to_count.values() and 2 in char_to_count.values():
-            return HandType.FULL_HOUSE
-        if 3 in char_to_count.values():
-            return HandType.THREE_OF_A_KIND
-        if len(set(hand)) == 3:
-            return HandType.TWO_PAIR
-        if len(set(hand)) == 4:
-            return HandType.ONE_PAIR
-        return HandType.HIGH_CARD
-
-    def sort_hands_by_type(self) -> dict[HandType, list[tuple[str, int]]]:
+    @property
+    def types_to_hand_list(self) -> dict[HandType, list[Hand]]:
         """
         Sort hands into their respective hand types.
         :return: A dict of hand types and their corresponding hands.
         """
         out = {hand_type: [] for hand_type in HandType}
-        for hand, bet in self._hands:
-            out[self.get_hand_type(hand)].append((hand, bet))
+        for hand in self._hands:
+            out[hand.hand_type].append(hand)
         return out
 
-    def sort_hands_by_strength(self, hands: list[tuple[str, int]]) -> list[tuple[str, int]]:
+    def sort_by_strength(self) -> None:
         """
         Sort the list of hands by their strength from worst to best.
-        :param hands: The list of hands to sort.
         :return: The list of hands sorted by their strength.
         """
 
-    def sorted_hands(self) -> list[tuple[str, int]]:
+        for hand_type, hand_list in self._types_to_hand_list:
+            decorated = [(hand.cards_as_nums, hand) for hand in hand_list]
+            decorated.sort()
+            self._types_to_hand_list[hand_type] = [hand for (cards, hand) in decorated]
+
+    def sorted_hands(self) -> list[Hand]:
         """
         Sort hands within each hand type and return all hands sorted from worst to best.
         :return:
         """
         out = []
         for hand_type in HandType:
-            out.extend(self.sort_hands_by_strength(self.sort_hands_by_type()[hand_type]))
+            out.extend(self._types_to_hand_list[hand_type])
         return out
 
     def solution(self) -> int:
         out = 0
-
-        for idx, _, bid in enumerate(self.sorted_hands()):
+        for idx, hand in enumerate(self.sorted_hands()):
             strength = idx + 1
-            out += bid * strength
+            out += hand.bid * strength
         return out
