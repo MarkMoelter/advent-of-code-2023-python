61ad8466ff8767b948ed648020ed69ed66a48a9b
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-10 00:19:47 -0500

    Add hand.py

--- src/Day7/part_1/__init__.py
+++ src/Day7/part_1/__init__.py
@@ -1,2 +1,3 @@
+from .hand import Hand
 from .hand_type import HandType
 from .part_1 import Part1
---
+++ src/Day7/part_1/hand.py
@@ -0,0 +1,60 @@
+from collections import Counter
+
+from .hand_type import HandType
+
+
+class Hand:
+    def __init__(self, cards: str, bid: int = 1) -> None:
+        self.cards = cards
+        self.bid = bid
+        self._cards_as_nums = self.cards_as_nums
+        self._hand_type = self.hand_type
+
+    @property
+    def cards_as_nums(self) -> list[int]:
+        """
+        Convert all alphabetical cards to their corresponding numerical value.
+        :return: A list of cards with corresponding numbers in the place of letters.
+        """
+
+        letter_to_int = {"T": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+
+        out = []
+        for card in self.cards:
+            if card.isalpha():
+                out.append(letter_to_int[card])
+            else:
+                out.append(int(card))
+
+        return out
+
+    @property
+    def hand_type(self) -> HandType:
+        """
+        Get the type of the hand.
+
+        Possible types:
+
+        - five of a kind
+        - Four of a kind
+        - Full house
+        - Three of a kind
+        - Two pair
+        - One pair
+        - High card
+        :return: The type of the hand.
+        """
+        char_to_count = Counter(self.cards)
+        if 5 in char_to_count.values():
+            return HandType.FIVE_OF_A_KIND
+        if 4 in char_to_count.values():
+            return HandType.FOUR_OF_A_KIND
+        if 3 in char_to_count.values() and 2 in char_to_count.values():
+            return HandType.FULL_HOUSE
+        if 3 in char_to_count.values():
+            return HandType.THREE_OF_A_KIND
+        if len(set(self.cards)) == 3:
+            return HandType.TWO_PAIR
+        if len(set(self.cards)) == 4:
+            return HandType.ONE_PAIR
+        return HandType.HIGH_CARD
