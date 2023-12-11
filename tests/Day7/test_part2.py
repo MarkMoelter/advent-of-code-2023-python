9c699e56dd3bb1b94f41f809d92c5041f1deae36
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-11 12:29:32 -0500

    Add unit tests for part 2

--- tests/Day7/test_part2.py
+++ tests/Day7/test_part2.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.Day7.part_2 import Part2
+from src.Day7.part_2 import Part2, JokerHand
 from src.read_file import read_file_lines
 
 
@@ -13,3 +13,14 @@ class TestPart2(unittest.TestCase):
         result = self.p2.solution()
 
         self.assertEqual(5905, result)
+
+    def test_sorted_hands_returns_correct_order(self):
+        result = [hand.cards for hand in self.p2.sorted_hands()]
+
+        self.assertListEqual(["32T3K", "KK677", "T55J5", "QQQJA", "KTJJT"], result)
+
+    def test_hands_property_returns_list_of_joker_hands(self):
+        result = self.p2.hands
+
+        for val in result:
+            self.assertIsInstance(val, JokerHand)
