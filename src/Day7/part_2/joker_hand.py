b4214de0f7a9cd3c7f3dfef0c1dc2e996f5dc9dd
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-11 09:54:59 -0500

    Add logic to get a hand's type

--- src/Day7/part_2/joker_hand.py
+++ src/Day7/part_2/joker_hand.py
@@ -1,5 +1,6 @@
-from Day7.part_1 import HandType
-from src.Day7.part_1 import Hand
+from collections import Counter
+
+from src.Day7.part_1 import Hand, HandType
 
 
 class JokerHand(Hand):
@@ -18,8 +19,36 @@ class JokerHand(Hand):
 
     @property
     def hand_type(self) -> HandType:
-        return super().hand_type
+        if "J" not in self.cards:
+            return super().hand_type
+        if self.cards == "JJJJJ":
+            return HandType.FIVE_OF_A_KIND
 
+        hand_count = Counter(self._cards_as_nums)
 
+        # Get the card with the highest count
+        max_card, max_count = 0, 0
+        for card, count in hand_count.items():
+            if card == self.joker_value:
+                continue
+            if count > max_count:
+                max_card, max_count = card, count
 
+        hand_count[max_card] += hand_count[self.joker_value]
 
+        if 5 in hand_count.values():
+            return HandType.FIVE_OF_A_KIND
+        if 4 in hand_count.values():
+            return HandType.FOUR_OF_A_KIND
+        if 3 in hand_count.values() and 2 in hand_count.values():
+            return HandType.FULL_HOUSE
+        if 3 in hand_count.values():
+            return HandType.THREE_OF_A_KIND
+        if len(set(self.cards)) == 3:
+            return HandType.TWO_PAIR
+        if len(set(self.cards)) == 4:
+            return HandType.ONE_PAIR
+        return HandType.HIGH_CARD
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}; {self.letter_to_int}, {self._cards_as_nums}"
