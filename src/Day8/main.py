7893b5ab778baea615a31b31440b49a2c8005d22
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-11 14:57:43 -0500

    Finish Day 8 Part 1

--- src/Day8/main.py
+++ src/Day8/main.py
@@ -1,19 +1,21 @@
 import logging
 
+from Day8.part_1 import Part1
+from Day8.part_2 import Part2
 from read_file import read_file_lines
-from template.part_1 import Part1
-from template.part_2 import Part2
 
 
 def main():
     input_file = read_file_lines()
-    input_file = read_file_lines("test_input.txt")
+    # input_file = read_file_lines("test_input.txt")
 
     # part 1
     p1 = Part1(input_file)
+    print(p1.solution())
 
     # part 2
     p2 = Part2(input_file)
+    # print(p2.solution())
 
 
 if __name__ == '__main__':
