7a7c8c00f561a99887dee25e79167afd75461232
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-11 15:27:45 -0500

    test: add unit tests for Part 2

---
+++ tests/Day8/test_part2.py
@@ -0,0 +1,16 @@
+import unittest
+
+from src.Day8.part_2 import Part2
+from src.read_file import read_file_lines
+
+
+class TestPart1(unittest.TestCase):
+    def setUp(self) -> None:
+        file_path = r"C:\Users\marke\PycharmProjects\advent-of-code-2023-python\src\Day8\test_input_part_2.txt"
+        self.p2 = Part2(
+            read_file_lines(file_path))
+
+    def test_solution_test_input_returns_6(self):
+        result = self.p2.solution("A", "Z")
+
+        self.assertEqual(6, result)
