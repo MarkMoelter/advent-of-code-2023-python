42c0771ac03d8cd8a9390b0e54f7f76f1046ef75
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-17 15:16:57 -0500

    feat: add Day 10 and test directory

---
+++ tests/Day10/test_part1.py
@@ -0,0 +1,16 @@
+import unittest
+
+from src.Day10.part_1 import Part1
+from src.read_file import read_file_lines
+
+
+class TestPart1(unittest.TestCase):
+    def setUp(self) -> None:
+        file_path = r"C:\Users\marke\PycharmProjects\advent-of-code-2023-python\src\Day10\test_input.txt"
+        self.p1 = Part1(
+            read_file_lines(file_path))
+
+    def test_solution_test_input_returns_8(self):
+        result = self.p1.solution()
+
+        self.assertEqual(8, result)
