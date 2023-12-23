951d780d89f2973c8d589d6b5486c2ba9bcc56d2
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-12-23 16:17:45 -0500

    feat: add direction.py

--- src/Day10/part_1/__init__.py
+++ src/Day10/part_1/__init__.py
@@ -1,2 +1,3 @@
+from .direction import Direction
 from .part_1 import Part1
 from .pipe import Pipe
---
+++ src/Day10/part_1/direction.py
@@ -0,0 +1,8 @@
+from enum import Enum, auto
+
+
+class Direction(Enum):
+    NORTH = auto()
+    SOUTH = auto()
+    EAST = auto()
+    WEST = auto()
